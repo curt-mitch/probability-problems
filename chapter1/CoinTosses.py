8a3ac960bcc262d0f7a7384a5d6fb4483fd5e4ba
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-01-04 21:31:33 -0800

    Chapter 1 exercise 1

---
+++ chapter1/CoinTosses.py
@@ -0,0 +1,18 @@
+import random
+
+# problem 1
+def coinTosses():
+  number = input("Number of times to flip the coin:")
+  recordCount = { "T": 0, "H": 0 }
+  heads = 0
+  tails = 0
+  for amount in range(number):
+    flip = random.randint(0, 1)
+    if (flip == 0):
+      recordCount["T"] += 1
+    else:
+      recordCount["H"] += 1
+    if (amount and amount % 100 == 0):
+      print "proportion of heads: " + str(1.0 * recordCount["T"] / amount - 0.5)
+
+coinTosses()
